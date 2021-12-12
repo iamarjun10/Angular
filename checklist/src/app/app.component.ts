58b3f9c3c9854333b673b0be27a636fb23ace138
iamarjun10 <naikarjun10@gmail.com>
2021-12-12 20:52:17 +0530

    Angular Practice Projects

---
+++ checklist/src/app/app.component.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'checklist';
+
+  selectedItemsList:any = [];
+  checkedIDs:any = [];
+
+  checkboxesDataList = [
+    {
+      id: 'C001',
+      label: 'Photography',
+      isChecked: true
+    },
+    {
+      id: 'C002',
+      label: 'Writing',
+      isChecked: true
+    },
+    {
+      id: 'C003',
+      label: 'Painting',
+      isChecked: true
+    },
+    {
+      id: 'C004',
+      label: 'Knitting',
+      isChecked: false
+    },
+    {
+      id: 'C004',
+      label: 'Dancing',
+      isChecked: false
+    },
+    {
+      id: 'C005',
+      label: 'Gardening',
+      isChecked: true
+    },
+    {
+      id: 'C006',
+      label: 'Drawing',
+      isChecked: true
+    },
+    {
+      id: 'C007',
+      label: 'Gyming',
+      isChecked: false
+    },
+    {
+      id: 'C008',
+      label: 'Cooking',
+      isChecked: true
+    },
+    {
+      id: 'C009',
+      label: 'Scrapbooking',
+      isChecked: false
+    },
+    {
+      id: 'C010',
+      label: 'Origami',
+      isChecked: false
+    }
+  ]
+
+  displaySelection() {
+    this.fetchSelectedItems()
+    this.fetchCheckedIDs();
+  }
+
+  fetchSelectedItems() {
+    this.selectedItemsList = this.checkboxesDataList.filter((value, index) => {
+      return value.isChecked
+    });
+    
+  }
+
+  fetchCheckedIDs() {
+    this.checkedIDs = []
+    this.checkboxesDataList.forEach((value, index) => {
+      if (value.isChecked) {
+        this.checkedIDs.push(value.id);
+      }
+    });
+  }
+
+  deleteSelected(){
+    //this.fetchSelectedItems()
+    //this.checkboxesDataList = this.checkboxesDataList.filter(item => item.id!== this.checkedIDs);
+  }
+
+  ngOnInit(): void {
+    this.fetchSelectedItems()
+    this.fetchCheckedIDs()
+  }
+
+}
